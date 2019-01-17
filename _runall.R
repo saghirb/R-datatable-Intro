d8792fa9187a0bf0dbac8b5fb157995cc6ed2941
Saghir Bashir <saghir@ilustat.com>
2019-01-17 18:42:03 +0000

    Updated _runall.R to (1) run all R markdown files and (2) Create the zip files so share with participants.

--- _runall.R
+++ _runall.R
@@ -14,12 +14,20 @@ rmarkdown::render(here("Exercises", "World-Popn", "World-Population-Exercises.Rm
 rmarkdown::render(here("Exercises", "World-Popn", "World-Population-Solutions.Rmd"),
                   clean = TRUE, output_dir = here("Exercises", "World-Popn"))
 
-# Create zip file to share with participants
+# Create zip files to share with participants
+# First empty the share folder and recreate the directory structure.
+unlink(here("Share/*"), recursive = TRUE)
+dir.create(here("Share", "R-Concepts"))
+dir.create(here("Share", "Slides-Notes"))
+dir.create(here("Share", "World-Popn"))
+
+# Populate the Share directories
 file.copy(here("Exercises", "Base-R", "Base-R-Exercises.pdf"),
           here("Share", "R-Concepts"), overwrite = TRUE)
 
 file.copy(here("Presentation", "R-and-data.table-Workshop.html"),
           here("Share", "Slides-Notes"), overwrite = TRUE)
+
 download.file("https://github.com/eddelbuettel/gsir-te/raw/master/Getting-Started-in-R.pdf",
               here("Share", "Slides-Notes", "Getting-Started-in-R.pdf"))
 
@@ -29,10 +37,14 @@ file.copy(here("Exercises", "World-Popn", "World-Population-Exercises.Rmd"),
 file.copy(here("Exercises", "World-Popn", "World-Population.csv"),
           here("Share", "World-Popn"), overwrite = TRUE)
 
-zip(here("Share", "GSiRdt.zip"), "Share/", extras = "-FS")
+rstudioapi::initializeProject(path = here("Share", "World-Popn"))
 
-# file.copy(here("Exercises", "World-Popn", "World-Population-Solutions.Rmd"),
-#           here("Share", "Solutions"), overwrite = TRUE)
-#
-# file.copy(here("Exercises", "World-Popn", "World-Population-Solutions.html"),
-#           here("Share", "Solutions"), overwrite = TRUE)
+# Using here() function with zip results in full paths in the zip files :(
+# Not beautiful: Using setwd to overcome the full paths issue above.
+setwd(here())
+zip(here("Share", "GSiRdt.zip"), "./Share/", extras = "-FS")
+
+setwd(here("Exercises", "World-Popn"))
+zip(here("Share", "World-Popn-solutions.zip"), "World-Population-Solutions.html",
+    extras = "-FS")
+setwd(here())
